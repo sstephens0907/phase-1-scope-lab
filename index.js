4fc696d6f4480542d8ece1fcdd36f8eeaaf848dd
Sarah Stephens <sarahstephens84@gmail.com>
2022-09-25 18:18:59 -0400

    commit tree 742f97e2db7e88f0ce289124c5ddcc360c0f5f81

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+var customerName = 'bob'
+
+function upperCaseCustomerName() {
+    return customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+    bestCustomer = 'not bob'
+    return setBestCustomer;
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+    return overwriteBestCustomer;
+}
+
+const leastFavoriteCustomer = 'Joe'
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'Sam'
+    return changeLeastFavoriteCustomer;
+}
